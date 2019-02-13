d733d520e74629fd7d021e67fbf19cd85d8ea3b5
fionagsweeney <fiona.sweeney@stjlabs.com>
2019-02-13 08:57:30 -0500

    finished array list minilab

--- ArrayListProject/src/ArrayListMiniLab.java
+++ ArrayListProject/src/ArrayListMiniLab.java
@@ -1,13 +1,46 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListMiniLab {
 
+private static ArrayList<Integer> factors = new ArrayList<Integer>();
+private static ArrayList<Integer> compFactors = new ArrayList<Integer>();
+
 public static void main(String[] args)
 {
-	isInt(7);
+	getFactors();
+	keepComps();
 }
 
-public static void isInt(int n)	
+public static void getFactors()
+{
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter an integer");
+	int num = input.nextInt();
+	for(int i = 2; i < num-1; i++)
+	{
+		if(num % i == 0 )
+		{
+			factors.add(i);
+		}
+	}
+	System.out.println("Factors: " + factors);
+}
+
+public static void keepComps()
+{
+	for(int i = 0; i < factors.size(); i++)
+	{
+		int newNum = factors.get(i);
+		if(isPrime(newNum) == false)
+		{
+			compFactors.add(newNum);
+		}
+	}
+	System.out.println("Composite Factors: " + compFactors);
+}
+
+public static boolean isPrime(int n)	
 {
 	int result = 1;
 	boolean isPrime = false;
@@ -19,6 +52,6 @@ public static void isInt(int n)
 	{
 		isPrime = true;
 	}
-	System.out.println(isPrime);
+	return isPrime;
 }
 }
